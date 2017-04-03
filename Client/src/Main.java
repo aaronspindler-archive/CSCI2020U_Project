ea6c977f273206936aa78a2b7300e60065e5e7e6
xNovax <aaron@xnovax.net>
2017-04-02 22:06:10 -0400

    Made basic UI

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -3,8 +3,8 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.GridPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -15,8 +15,13 @@ import java.io.File;
 
 
 public class Main extends Application {
-    Button play;
+    MenuBar menuBar;
+    Menu fileMenu;
+    MenuItem exitMenuItem;
+    Button play, prev, next;
     Slider timeSlider;
+    ListView<Song> songList;
+
     MediaPlayer mediaPlayer;
     Duration time;
     Boolean isPlaying = false;
@@ -28,18 +33,60 @@ public class Main extends Application {
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.TOP_LEFT);
 
+        menuBar = new MenuBar();
+        fileMenu = new Menu("File");
+
+        exitMenuItem = new MenuItem("Exit");
+        exitMenuItem.setAccelerator(KeyCombination.keyCombination("Ctrl+Q"));
+        exitMenuItem.setOnAction(e -> System.exit(0));
+
+        fileMenu.getItems().add(exitMenuItem);
+        menuBar.getMenus().add(fileMenu);
+
+        grid.add(menuBar,0,0,12,1);
+
+        songList = new ListView<>();
+        songList.setMinWidth(150);
+        songList.setMaxWidth(150);
+        grid.add(songList, 0, 1, 3,1);
+
+        prev = new Button("<<");
+        prev.setMinWidth(50);
+        prev.setMaxWidth(50);
+        grid.add(prev, 0, 2, 1,1);
+
         play = new Button(">");
+        play.setMinWidth(50);
+        play.setMaxWidth(50);
         play.setOnAction(e -> play());
+        grid.add(play,1,2,1,1);
+
+        next = new Button(">>");
+        next.setMinWidth(50);
+        next.setMaxWidth(50);
+        grid.add(next, 2, 2, 1, 1);
+
         timeSlider = new Slider();
+        timeSlider.setMinWidth(300);
+        timeSlider.setMaxWidth(300);
+        grid.add(timeSlider, 3,2,3,1);
+
         String file = "excellent.mp3";
         Media song = new Media(new File(file).toURI().toString());
         mediaPlayer = new MediaPlayer(song);
-        grid.add(play,0,0,1,1);
-        grid.add(timeSlider, 0,2,2,1);
+
 
         Scene scene = new Scene(grid, 450,400);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public void prev(){
+
+    }
+
+    public void next(){
+
     }
 
     public void play() {
